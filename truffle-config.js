68ccb12337771032e23f9dc32f812f6aef706847
Miguel Carvajal Hang <miguel.hang@gmail.com>
2019-03-21 15:09:10 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const mnemonic = "relax believe two obtain mixture lava fortune mirror panther crowd disagree pyramid";
+const mnemonic = "";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -11,7 +11,7 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/2f9a138faf9b44a7845398a615ef95f6'),
+      provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/'),
       network_id: 3, // Ropsten's id
       gas: 5500000, // Ropsten has a lower block limit than mainnet
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
